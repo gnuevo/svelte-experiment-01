250b5c32f9e0013bd90921d4c50518ca316d9b68
Gregorio Nuevo Castro <gncdvp@gmail.com>
2025-02-06 14:47:32 +0100

    Implement dropdown in and out transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import { fade, scale } from 'svelte/transition';
 
 	/** @type {{children: import('svelte').Snippet}} */
 	let { children } = $props();
@@ -70,13 +71,17 @@
 					From: "transform opacity-100 scale-100"
 					To: "transform opacity-0 scale-95"
 				-->
-				<div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden" 
-					role="menu" class:dropdown={true} class:open={isDropdownOpen} aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+				{#if isDropdownOpen}
+				<div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden
+				 	dropdown transition transform {isDropdownOpen ? 'ease-out opacity-100 scale-100' : 'ease-in opacity-0 scale-95'}"
+					in:fade={{ duration: 100 }} out:fade={{ duration: 75 }}
+					role="menu" class:open={isDropdownOpen} aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
 				  <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->
 				  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
 				  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
 				  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
 				</div>
+				{/if}
 			  </div>
 			</div>
 		  </div>
